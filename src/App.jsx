e168ea0b9ce53202de2283c943b39bc204290a5a
Andrew <andrew.orodenker@gmail.com>
2023-05-08 12:50:00 -0700

    loading bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Canvas, useThree } from '@react-three/fiber';
-import { OrbitControls, Environment } from '@react-three/drei';
-import { useState, useEffect, useRef } from 'react';
+import { OrbitControls, Environment, Loader } from '@react-three/drei';
+import { useState, useEffect, useRef, Suspense } from 'react';
 import { Perf } from 'r3f-perf';
 import { starAssets } from './Utils/starAssets.js';
 import Planets from './components/Planets';
@@ -8,10 +8,12 @@ import TextDisplay from './components/TextDisplay';
 import Navigation from './components/Navigation';
 
 const App = () => {
+  const [loaded, setLoaded] = useState(false);
   const [selected, setSelected] = useState(0);
   const [content, setContent] = useState(0);
   const [hidden, setHidden] = useState(false);
   const [controls, setControls] = useState(true);
+  console.log(loaded)
 
   useEffect(() => {
     if (selected === 0) {
@@ -32,14 +34,17 @@ const App = () => {
         position: [-157, 151, -139],
       }}
     >
-      {/* <Perf position="top-left" /> */}
-      <OrbitControls makeDefault enableRotate={controls} />
-      <pointLight color="white" position={[0, 0, 0]} intensity={1} distance={3000} />
-      <ambientLight intensity={0.05} />
+      <Suspense fallback={null}>
+        {/* <Perf position="top-left" /> */}
+        <OrbitControls makeDefault enableRotate={controls} />
+        <pointLight color="white" position={[0, 0, 0]} intensity={1} distance={3000} />
+        <ambientLight intensity={0.05} />
 
-      <Environment background files={[...starAssets]} blur={0.2} />
-      <Planets selected={selected} setHidden={setHidden} />
+        <Environment background files={[...starAssets]} blur={0.2} />
+        <Planets selected={selected} setHidden={setHidden} onComplete={() => setLoaded(true)} />
+      </Suspense>
     </Canvas>
+    <Loader />
     <TextDisplay content={content} setContent={setContent} hidden={hidden} setHidden={setHidden} />
     </>
   );
