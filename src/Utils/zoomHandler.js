b35dd5eb9d8f408be45b8ccb67a3085ac76e013e
Andrew <andrew.orodenker@gmail.com>
2023-05-17 12:30:33 -0700

    added gtag

--- src/Utils/zoomHandler.js
+++ src/Utils/zoomHandler.js
@@ -44,6 +44,8 @@ const zoomHandler = (state, delta, idx, ref) => {
 
   const currentPos = camera.position.clone();
   const currentTarget = mesh.position.clone();
+  console.log(mesh.translation())
+  console.log(currentTarget)
   const dir = currentTarget.sub(currentPos);
   const dist = dir.length();
   dir.normalize();
