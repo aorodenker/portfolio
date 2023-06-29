b5e7d7a7089f02a4e5116f93bcd7836c9ccd7f9e
Andrew <andrew.orodenker@gmail.com>
2023-06-29 12:41:08 -0700

    rotationcalc typo

--- src/Utils/rotationCalc.js
+++ src/Utils/rotationCalc.js
@@ -1,9 +1,9 @@
 const rotationCalc = (delta, ref) => {
   const standardRotation = 0.262512;
 
-  ref[0].rotation.y += delta * (standardRotation);
-  ref[1].rotation.y += delta * (standardRotation);
-  ref[2].rotation.y += delta * (standardRotation);
+  ref[0].rotation.y += delta * standardRotation;
+  ref[1].rotation.y += delta * standardRotation;
+  ref[2].rotation.y += delta * standardRotation;
   ref[3].rotation.y += delta * standardRotation;
   ref[4].rotation.y += delta * (standardRotation / 1.03);
   ref[5].rotation.y += delta * (standardRotation / 0.41);
